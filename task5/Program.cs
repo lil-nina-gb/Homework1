2918c05ea99c02f9f446b2703f588fa5f133d496
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-06 21:44:36 +0700

    added task5

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите длину: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
