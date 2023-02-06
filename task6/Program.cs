a8f3ba0055ce323545d79070dd9b562a3ecaea95
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-06 21:44:49 +0700

    added task6

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + numberText[2]); 
+}
