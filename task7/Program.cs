31075547bad43f540aca084bbaf1b0f612898e37
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-06 21:45:00 +0700

    added task7

---
+++ task7/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать вторую цифру трёхзначного числа 
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length > 1)
+Console.WriteLine("Вторая цифра ->"+numberText[1]);
+
