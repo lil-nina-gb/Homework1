a46865e99befa935678e87745735a1a3b77da627
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-06 21:43:16 +0700

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 / number2 == number2) 
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+
+
+ else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
+
