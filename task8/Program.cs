88077f22263cf63207b01a37957f1b83fec354d2
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-06 22:43:50 +0700

    added task8

---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 99);
+Console.WriteLine(number);
+if (number / 10 > number % 10)
+Console.WriteLine("Первая цифра больше");
+else
+ Console.WriteLine("Вторая цифра больше");
