aa43e02886400abe3603f17df4d8c706735ca5c0
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-06 21:44:14 +0700

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿//Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+    Console.WriteLine("Число четное");
+else 
+Console.WriteLine("Число нечетное");
